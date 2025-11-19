09a96c51201875b432387278ad9d20b0a8099599
Zachary Kerby <Zckerby@gmail.com>
2025-11-19 01:07:49 -0800

    added some keymaps IW

---
+++ lua/zckerby/setting/keymap.lua
@@ -0,0 +1,20 @@
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>")   --open new tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>") --close current tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>")     --go to next
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>")     --go to pre
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>") --open current tab in new tab
+
+
+local isLspDiagnosticsVisible = true
+vim.keymap.set("n", "<leader>lx", function()
+    isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+    vim.diagnostic.config({
+        virtual_text = isLspDiagnosticsVisible,
+        underline = isLspDiagnosticsVisible
+    })
+end, { desc = "Toggle LSP diagnostics" })
