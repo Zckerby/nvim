1f5cf3372cb888f6c08bb05705d4acff89fcb251
Zachary <zckerby@gmail.com>
2026-03-12 19:09:03 -0700

    added telescope capability TODO: Fix FG issues

--- lua/zckerby/version2/telescope.lua
+++ lua/zckerby/version2/telescope.lua
@@ -1,18 +1,21 @@
 vim.pack.add({
     {
-        src = 'https://github.com/nvim-telescope/telescope.nvim',
-        version = '*',
+        src = "https://github.com/nvim-telescope/telescope.nvim",
+        version = "v0.2.1",
     },
-    'https://github.com/nvim-lua/plenary.nvim',
+    "https://github.com/nvim-lua/plenary.nvim",
     {
-        src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
+        src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
     },
 })
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+require("telescope").setup()
+pcall(require("telescope").load_extension, "fzf")
 
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
